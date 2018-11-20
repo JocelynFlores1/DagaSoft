ee5a789441ee95c8ee647e4a2d14374caaef7810
Daniela1313 <38969496+Daniela1313@users.noreply.github.com>
2018-11-19 20:33:53 -0600

    A cada boton se le agrego un ActionListener

--- src/Controllers/ControllerEmpleados.java
+++ src/Controllers/ControllerEmpleados.java
@@ -7,12 +7,15 @@ package Controllers;
 
 import Models.ModelEmpleados;
 import Views.ViewEmpleados;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
  * @author beto
  */
 public class ControllerEmpleados {
+
     public Models.ModelEmpleados modelEmpleados;
     public Views.ViewEmpleados viewEmpleados;
 
@@ -20,5 +23,36 @@ public class ControllerEmpleados {
         this.modelEmpleados = modelEmpleados;
         this.viewEmpleados = viewEmpleados;
     }
-    
+
+    /**
+     * Objeto de tipo ActionListener para atrapar los eventos actionPerformed y
+     * llamar a los metodos para ver los registros almacenados en la bd.
+     */
+    ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == viewEmpleados.jb_nuevo) {
+                jmi_nuevo();
+            }
+            if (e.getSource() == viewEmpleados.jb_modificar) {
+                jmi_modificar();
+            }
+            if (e.getSource() == viewEmpleados.jb_insertar) {
+                jmi_insertarE();
+            }
+            if (e.getSource() == viewEmpleados.jb_eliminar) {
+                jmi_eliminar();
+            } else if (e.getSource() == viewEmpleados.jb_cancelar) {
+
+            }
+        }
+    };
+
+    public void setActionListener() {
+        viewEmpleados.jb_nuevo.addActionListener(actionListener);
+        viewEmpleados.jb_cancelar.addActionListener(actionListener);
+        viewEmpleados.jb_modificar.addActionListener(actionListener);
+        viewEmpleados.jb_insertar.addActionListener(actionListener);
+        viewEmpleados.jb_eliminar.addActionListener(actionListener);
+    }
 }
