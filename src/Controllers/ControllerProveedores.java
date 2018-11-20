a190af700954dc2eabcb65dca49fbd9d87938b4a
JocelynFlores1 <37678163+JocelynFlores1@users.noreply.github.com>
2018-11-20 00:29:39 -0600

    Con este metodo se pueden modificar los datos del proveedor

--- src/Controllers/ControllerProveedores.java
+++ src/Controllers/ControllerProveedores.java
@@ -57,8 +57,8 @@ public class ControllerProveedores {
         modelProveedores.conectarDB();
         jtfCambiarCampos();
     }
-    
-     /**
+
+    /**
      * *
      * Metodo que cambia los campos de los jTextField en ViewProveedores
      */
@@ -73,9 +73,9 @@ public class ControllerProveedores {
         viewProveedores.jtf_email.setText(modelProveedores.getEmail_proveedor());
         viewProveedores.jtf_ciudad.setText(modelProveedores.getCiudad_proveedor());
         viewProveedores.jtf_estado.setText(modelProveedores.getEstado_proveedor());
-    } 
-    
-       /**
+    }
+
+    /**
      * Método para agregar el actionListener a cada boton de la vista
      */
     public void setActionListener() {
@@ -91,8 +91,8 @@ public class ControllerProveedores {
             }
         });
     }
-    
-       /**
+
+    /**
      * *
      * Modelo que permite insertar el texto obenido en los jTextField de
      * ViewClientes e inserta los valores en las variables de ModelProveedores
@@ -119,5 +119,27 @@ public class ControllerProveedores {
             JOptionPane.showMessageDialog(null, "No se guardo ningún registro");
         }
     }
-    
+
+    public void jmi_modificarP_actionPerformed() {
+        //JOptionPane.showConfirmDialog permite al usuario elegir si realizar la accion del boton solicitado o simplemente cancelarlo
+        int cancelar = JOptionPane.showConfirmDialog(null, "¿Desea actualizar los datos del proveedor?", "Guardar cambios", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (cancelar == 0) {
+            modelProveedores.setId_proveedor(viewProveedores.jtf_id_proveedor.getText());
+            modelProveedores.setNombre_proveedor(viewProveedores.jtf_nombre_proveedor.getText());
+            modelProveedores.setCalle_proveedor(viewProveedores.jtf_calle.getText());
+            modelProveedores.setNumero_exterior_proveedor(Integer.parseInt(viewProveedores.jtf_numero_exterior.getText()));
+            modelProveedores.setColonia_proveedor(viewProveedores.jtf_colonia.getText());
+            modelProveedores.setCodigo_postal_proveedor(Integer.parseInt(viewProveedores.jtf_codigo_postal.getText()));
+            modelProveedores.setTelefono_proveedor(viewProveedores.jtf_telefono.getText());
+            modelProveedores.setEmail_proveedor(viewProveedores.jtf_email.getText());
+            modelProveedores.setCiudad_proveedor(viewProveedores.jtf_ciudad.getText());
+            modelProveedores.setEstado_proveedor(viewProveedores.jtf_estado.getText());
+
+            modelProveedores.modificarDatosProveedor();
+
+        } else {
+            ///Respuesta que se obtiene cuando se cancela la accion del boton elegido
+            JOptionPane.showMessageDialog(null, "No se guardo ningun cambio");
+        }
+    }
 }
