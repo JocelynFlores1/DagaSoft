aaceaf65f29daa0808ff1e75b10830490fe70054
Daniela1313 <38969496+Daniela1313@users.noreply.github.com>
2018-11-19 19:51:35 -0600

    Se agregaron las variables que se van a ocupar para empleados

--- src/Models/ModelEmpleados.java
+++ src/Models/ModelEmpleados.java
@@ -5,10 +5,215 @@
  */
 package Models;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
- * @author beto
+ * @author Dany
  */
 public class ModelEmpleados {
-    
+
+    public Connection conexion;
+    public Statement st;
+    public ResultSet rs;
+    public PreparedStatement ps;
+
+    public String id_empleado;
+    public String nombre_empleado;
+    public String apellido_paterno;
+    public String apellido_materno;
+    public String telefono;
+    public String calle;
+    public String colonia;
+    public String numero_exterior;
+    public String numero_interior;
+    public String rfc;
+    public String no_cueta;
+    public String no_seguro;
+    public String banco;
+    public String curp;
+    public String tipo_empleado;
+    public String usuario;
+    public String contrasena;
+    public String codigo_postal;
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+    public Statement getSt() {
+        return st;
+    }
+
+    public void setSt(Statement st) {
+        this.st = st;
+    }
+
+    public ResultSet getRs() {
+        return rs;
+    }
+
+    public void setRs(ResultSet rs) {
+        this.rs = rs;
+    }
+
+    public PreparedStatement getPs() {
+        return ps;
+    }
+
+    public void setPs(PreparedStatement ps) {
+        this.ps = ps;
+    }
+
+    public String getId_empleado() {
+        return id_empleado;
+    }
+
+    public void setId_empleado(String id_empleado) {
+        this.id_empleado = id_empleado;
+    }
+
+    public String getNombre_empleado() {
+        return nombre_empleado;
+    }
+
+    public void setNombre_empleado(String nombre_empleado) {
+        this.nombre_empleado = nombre_empleado;
+    }
+
+    public String getApellido_paterno() {
+        return apellido_paterno;
+    }
+
+    public void setApellido_paterno(String apellido_paterno) {
+        this.apellido_paterno = apellido_paterno;
+    }
+
+    public String getApellido_materno() {
+        return apellido_materno;
+    }
+
+    public void setApellido_materno(String apellido_materno) {
+        this.apellido_materno = apellido_materno;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String Calle) {
+        this.calle = calle;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.calle = calle;
+    }
+
+    public String getNumero_exterior() {
+        return numero_exterior;
+    }
+
+    public void setNumero_exterior(String numero_exterior) {
+        this.numero_exterior = numero_exterior;
+    }
+
+    public String getNumero_interior() {
+        return numero_interior;
+    }
+
+    public void setNumero_interior(String numero_interior) {
+        this.numero_interior = numero_interior;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String getNo_cueta() {
+        return no_cueta;
+    }
+
+    public void setNo_cueta(String no_cueta) {
+        this.no_cueta = no_cueta;
+    }
+
+    public String getNo_seguro() {
+        return no_seguro;
+    }
+
+    public void setNo_seguro(String no_seguro) {
+        this.no_seguro = no_seguro;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public String getCurp() {
+        return curp;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    public String getTipo_empleado() {
+        return tipo_empleado;
+    }
+
+    public void setTipo_empleado(String tipo_empleado) {
+        this.tipo_empleado = tipo_empleado;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getCodigo_postal() {
+        return codigo_postal;
+    }
+
+    public void setCodigo_postal(String codigo_postal) {
+        this.codigo_postal = codigo_postal;
+    }
+
 }
