784997b43b777d00e4ad67ee8ae596f790a4c5d4
JocelynFlores1 <37678163+JocelynFlores1@users.noreply.github.com>
2018-11-20 02:47:55 -0600

    Edicion de ModelProductos

--- src/Models/ModelProductos.java
+++ src/Models/ModelProductos.java
@@ -5,10 +5,204 @@
  */
 package Models;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author beto
  */
 public class ModelProductos {
-    
+
+    private Connection conexion;
+    private Statement st;
+    private ResultSet rs;
+    private PreparedStatement ps;
+
+    private String id_producto;
+    private String nombre_producto;
+    private String tipo_producto;
+    private String descripcion_producto;
+    private String marca_producto;
+    private Double precio_venta;
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+    public Statement getSt() {
+        return st;
+    }
+
+    public void setSt(Statement st) {
+        this.st = st;
+    }
+
+    public ResultSet getRs() {
+        return rs;
+    }
+
+    public void setRs(ResultSet rs) {
+        this.rs = rs;
+    }
+
+    public PreparedStatement getPs() {
+        return ps;
+    }
+
+    public void setPs(PreparedStatement ps) {
+        this.ps = ps;
+    }
+
+    public String getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(String id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public String getTipo_producto() {
+        return tipo_producto;
+    }
+
+    public void setTipo_producto(String tipo_producto) {
+        this.tipo_producto = tipo_producto;
+    }
+
+    public String getDescripcion_producto() {
+        return descripcion_producto;
+    }
+
+    public void setDescripcion_producto(String descripcion_producto) {
+        this.descripcion_producto = descripcion_producto;
+    }
+
+    public String getMarca_producto() {
+        return marca_producto;
+    }
+
+    public void setMarca_producto(String marca_producto) {
+        this.marca_producto = marca_producto;
+    }
+
+    public Double getPrecio_venta() {
+        return precio_venta;
+    }
+
+    public void setPrecio_venta(Double precio_venta) {
+        this.precio_venta = precio_venta;
+    }
+
+    public void modificarDatos() {
+        try {
+            setId_producto(rs.getString(1));
+            setNombre_producto(rs.getString(2));
+            setTipo_producto(rs.getString(3));
+            setDescripcion_producto(rs.getString(4));
+            setMarca_producto(rs.getString(5));
+            setPrecio_venta(rs.getDouble(6));
+        } catch (SQLException e) {
+            System.out.println("Error 01: modificar datos" + e);
+        }
+    }
+
+    /**
+     * Método que realiza las siguietnes acciones: 1.- Conecta con la base 2.-
+     * Consulta todo los registros .
+     */
+    public void conectarDB() {
+        ModelConexion loginConexion = new ModelConexion();
+        loginConexion.getConexion();
+        try {
+            String consultaString = "select * from productos";
+            ps = (PreparedStatement) loginConexion.getConexion().prepareStatement(consultaString);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                modificarDatos();
+            } else {
+                System.out.println("Error de consulta");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error 02: tabla productos" + e);
+        }
+    }
+
+    public void insertarNuevoCliente() {
+        ModelConexion loginConexion = new ModelConexion();
+        //Se obtiene la conexion para la clase
+
+        String sqlInsertarCliente = "insert into productos (id_producto, nombre_producto,tipo_producto,descripcion_producto,marca_producto, precio_venta_producto)"
+                + " VALUES(?,?,?,?,?,?)";
+        try {
+            ps = (com.mysql.jdbc.PreparedStatement) loginConexion.getConexion().prepareStatement(sqlInsertarCliente); //con este comando se podra hacer la modificacion a la tabla en la base de datos
+            System.out.println(getId_producto());
+            ps.setString(1, getId_producto());
+            ps.setString(2, getNombre_producto());
+            ps.setString(3, getTipo_producto());
+            ps.setString(4, getDescripcion_producto());
+            ps.setString(5, getMarca_producto());
+            ps.setDouble(6, getPrecio_venta());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("Error 03: Insertar nuevo producto" + ex);
+        }
+    }
+
+    public void modificarDatosProducto() {
+        ModelConexion loginConexion = new ModelConexion();
+        //Se obtiene la conexion para la clase
+
+        String sqlModificarProducto = "update productos set "
+                + "nombre_producto=?,  "
+                + "tipo_producto=?, descripcion_producto=?"
+                + "marca_producto=?, precio_venta_producto where id_producto = ?";
+        try {
+
+            ps = (com.mysql.jdbc.PreparedStatement) loginConexion.getConexion().prepareStatement(sqlModificarProducto);
+
+            System.out.println(getId_producto());
+
+            ps.setString(1, getNombre_producto());
+            ps.setString(2, getTipo_producto());
+            ps.setString(3, getDescripcion_producto());
+            ps.setString(4, getMarca_producto());
+            ps.setDouble(5, getPrecio_venta());
+            ps.setString(6, getId_producto());
+            ps.executeUpdate();
+
+        } catch (SQLException ex) {
+            System.out.println("Error 04: Modificar datos producto" + ex);
+        }
+    }
+    //Metodo que permite insertar los datos de la tabla de la base de datos en un jTable en java
+
+    public void consultajTableProductos() {
+        ModelConexion loginConexion = new ModelConexion();
+        loginConexion.getConexion();
+        try {
+            String consultaString = "select * from productos";
+            ps = (PreparedStatement) loginConexion.getConexion().prepareStatement(consultaString);
+            rs = ps.executeQuery();
+        } catch (SQLException e) {
+            System.out.println("Error 06: tabla productos" + e);
+        }
+    }
+
 }
